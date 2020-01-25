98e2d60d5536a13d643f92f82c9c68b5278f7bd2
leonelparrales22 <dparrales@est.ups.edu.ec>
2020-01-25 17:44:04 -0500

    Update Script_Proyecto.R

--- Script_Proyecto.R
+++ Script_Proyecto.R
@@ -1,2 +1,15 @@
 #PROYECTO ESTADISTICA PARA CIENCIAS DE LA COMPUTACION
 
+#Importacion del Dataset
+library(readr)
+dataset <- read_csv("Documentos/Estadistica_Proyecto/Dataset/dataset.csv")
+View(dataset)
+#Asignacion de las variables
+x1=dataset$MYCT
+x2=dataset$MMIN
+x3=dataset$MMAX
+x4=dataset$CACH
+x5=dataset$CHMIN
+x6=dataset$CHMAX
+x6=dataset$PRP
+y=dataset$ERP
